f12b14e9e088a7679541acf8cdb5f4470f8cc854
gaoshiyong1272 <gaoshiyong1272@vip.163.com>
2021-01-12 00:51:39 +0800

    正式发布

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') { // 通过环境变量来决定入口文件
-  module.exports = require('./lib/gxd.umd.min')
+  module.exports = require('./lib/gxdMin.umd.min')
 } else {
   module.exports = require('./lib/gxdSource.umd')
 }
